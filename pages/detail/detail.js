46eb874fa5745decad3adbecea203bb790212373
Sam Li <weisen.li@hotmail.com>
2019-10-21 17:25:18 -0400

    refactor detail page, onpulldownrefresh

--- pages/detail/detail.js
+++ pages/detail/detail.js
@@ -15,18 +15,9 @@ Page({
    */
   onLoad: function (options) {
     const id = options.id;
-    const self = this;
-    wx.request({
-      url: api+id,
-      header: {
-        'content-type': 'application/json' // 默认值
-      },
-      success(res) {
-        self.setData({ todo: res.data });
-      }
-    })
+    this.setData({todo:{_id:id}});
+    this.getTodo(id);
   },
-
   /**
    * Lifecycle function--Called when page is initially rendered
    */
@@ -59,7 +50,7 @@ Page({
    * Page event handler function--Called when user drop down
    */
   onPullDownRefresh: function () {
-
+    this.getTodo(this.data.todo._id);
   },
 
   /**
@@ -75,6 +66,19 @@ Page({
   onShareAppMessage: function () {
 
   },
+  getTodo(id) {
+    const self = this;
+    wx.request({
+      url: api + id,
+      header: {
+        'content-type': 'application/json' // 默认值
+      },
+      success(res) {
+        self.setData({ todo: res.data });
+        wx.stopPullDownRefresh();
+      }
+    })
+  },
   editTodo:function(e) {
     const todo = e.currentTarget.dataset.todo;
     wx.navigateTo({
@@ -88,6 +92,11 @@ Page({
       method:'DELETE',
       success(res) {
         wx.navigateBack({});
+        wx.showToast({
+          title: '删除成功',
+          icon: 'success',
+          duration: 2000
+        });
       }
     })
   }
