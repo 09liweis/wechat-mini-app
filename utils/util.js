1b794145d6d7808a233b11a7fdfbb0e15e54966c
Sam Li <weisen.li@hotmail.com>
2020-10-16 21:33:50 -0400

    add getCurrentDate method in util

--- utils/util.js
+++ utils/util.js
@@ -42,7 +42,21 @@ const wxRequest = (url,opt,cb) => {
   })
 }
 
+const getCurrentDate = () => {
+  const year = date.getFullYear();
+  let month = date.getMonth() + 1;
+  month = month > 9 ? month : '0' + month;
+  let day = date.getDate();
+  day = day > 9 ? day : '0' + day;
+  return {
+    year,
+    month,
+    day
+  }
+}
+
 module.exports = {
   formatTime: formatTime,
-  wxRequest: wxRequest
+  wxRequest: wxRequest,
+  getCurrentDate: getCurrentDate
 }
