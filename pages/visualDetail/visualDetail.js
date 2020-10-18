3b671c4495cfa8aabe27ad105b4077592a5f8e76
Sam Li <weisen.li@hotmail.com>
2020-10-18 14:59:39 -0400

    add visual detail js get data

--- pages/visualDetail/visualDetail.js
+++ pages/visualDetail/visualDetail.js
@@ -1,3 +1,4 @@
+const { wxRequest } = require('../../utils/util.js');
 // pages/visualDetail/visualDetail.js
 Page({
 
@@ -5,14 +6,17 @@ Page({
    * Page initial data
    */
   data: {
-
+    douban_id:'',
+    v:{}
   },
 
   /**
    * Lifecycle function--Called when page load
    */
   onLoad: function (options) {
-
+    let {douban_id} = options;
+    this.setData({douban_id:douban_id || '26100958'});
+    this.getVisualDetail();
   },
 
   /**
@@ -62,5 +66,22 @@ Page({
    */
   onShareAppMessage: function () {
 
+  },
+  getVisualDetail: function() {
+    const {douban_id} = this.data;
+    const url = 'https://samliweisen.herokuapp.com/api/visuals/summary';
+    const self = this;
+    wx.showLoading({
+      title: '加载数据中(^ o ^)',
+      mask:true
+    });
+    wxRequest(url,{method:'POST',data:{douban_id}},function(res){
+      wx.hideLoading();
+      const {statusCode,data} = res;
+      if (statusCode == 200) {
+        console.log(data);
+        self.setData({v:data});
+      }
+    });
   }
 })
