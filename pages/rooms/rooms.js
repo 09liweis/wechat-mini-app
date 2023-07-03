abe1a2385c22a1b25ef92e91fe8c20ecc1203b75
Sam Li <weisen.li@hotmail.com>
2023-07-03 14:50:03 -0400

    feat(rooms): initial rooms/room page

---
+++ pages/rooms/rooms.js
@@ -0,0 +1,75 @@
+const { wxRequest } = require("../../utils/util")
+
+// pages/rooms/rooms.js
+Page({
+
+  /**
+   * Page initial data
+   */
+  data: {
+    rooms:[]
+  },
+
+  /**
+   * Lifecycle function--Called when page load
+   */
+  onLoad(options) {
+    let url = 'https://samliweisen.onrender.com/api/rooms';
+    wxRequest(url,{},function(res){
+      console.log(res);
+    });
+
+    wxRequest('https://api.openweathermap.org/data/2.5/weather?appid=323b480b81057a727bed54d9532159d6&lat=43.8288928&lon=-79.2789297&units=metric',{},function(res){
+      console.log(res);
+    });
+  },
+
+  /**
+   * Lifecycle function--Called when page is initially rendered
+   */
+  onReady() {
+
+  },
+
+  /**
+   * Lifecycle function--Called when page show
+   */
+  onShow() {
+
+  },
+
+  /**
+   * Lifecycle function--Called when page hide
+   */
+  onHide() {
+
+  },
+
+  /**
+   * Lifecycle function--Called when page unload
+   */
+  onUnload() {
+
+  },
+
+  /**
+   * Page event handler function--Called when user drop down
+   */
+  onPullDownRefresh() {
+
+  },
+
+  /**
+   * Called when page reach bottom
+   */
+  onReachBottom() {
+
+  },
+
+  /**
+   * Called when user click on the top right corner to share
+   */
+  onShareAppMessage() {
+
+  }
+})
